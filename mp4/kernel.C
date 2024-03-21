78d61558e9599c7fca4d3a35963d12b91073f666
Jackson Stewart <jts3304@gmail.com>
2024-03-20 19:55:46 -0500

    use 4 spaces

--- mp4/kernel.C
+++ mp4/kernel.C
@@ -11,14 +11,13 @@
 
 */
 
-
 /*--------------------------------------------------------------------------*/
 /* DEFINES */
 /*--------------------------------------------------------------------------*/
 
-#define GB * (0x1 << 30)
-#define MB * (0x1 << 20)
-#define KB * (0x1 << 10)
+#define GB *(0x1 << 30)
+#define MB *(0x1 << 20)
+#define KB *(0x1 << 10)
 #define KERNEL_POOL_START_FRAME ((2 MB) / Machine::PAGE_SIZE)
 #define KERNEL_POOL_SIZE ((2 MB) / Machine::PAGE_SIZE)
 #define PROCESS_POOL_START_FRAME ((4 MB) / Machine::PAGE_SIZE)
@@ -38,20 +37,17 @@
 /* INCLUDES */
 /*--------------------------------------------------------------------------*/
 
-#include "machine.H"        /* LOW-LEVEL STUFF */
 #include "console.H"
-#include "gdt.H"
-#include "idt.H"            /* LOW-LEVEL EXCEPTION MGMT. */
-#include "irq.H"
 #include "exceptions.H"
+#include "gdt.H"
+#include "idt.H" /* LOW-LEVEL EXCEPTION MGMT. */
 #include "interrupts.H"
-
-#include "simple_keyboard.H" /* SIMPLE KB DRIVER */
-#include "simple_timer.H"   /* SIMPLE TIMER MANAGEMENT */
-
+#include "irq.H"
+#include "machine.H" /* LOW-LEVEL STUFF */
 #include "page_table.H"
 #include "paging_low.H"
-
+#include "simple_keyboard.H" /* SIMPLE KB DRIVER */
+#include "simple_timer.H"    /* SIMPLE TIMER MANAGEMENT */
 #include "vm_pool.H"
 
 /*--------------------------------------------------------------------------*/
@@ -74,26 +70,26 @@ VMPool *current_pool;
 
 typedef long unsigned int size_t;
 
-//replace the operator "new"
-void * operator new (size_t size) {
-  unsigned long a = current_pool->allocate((unsigned long)size);
-  return (void *)a;
+// replace the operator "new"
+void *operator new(size_t size) {
+    unsigned long a = current_pool->allocate((unsigned long)size);
+    return (void *)a;
 }
 
-//replace the operator "new[]"
-void * operator new[] (size_t size) {
-  unsigned long a = current_pool->allocate((unsigned long)size);
-  return (void *)a;
+// replace the operator "new[]"
+void *operator new[](size_t size) {
+    unsigned long a = current_pool->allocate((unsigned long)size);
+    return (void *)a;
 }
 
-//replace the operator "delete"
-void operator delete (void * p, size_t s) {
-  current_pool->release((unsigned long)p);
+// replace the operator "delete"
+void operator delete(void *p, size_t s) {
+    current_pool->release((unsigned long)p);
 }
 
-//replace the operator "delete[]"
-void operator delete[] (void * p) {
-  current_pool->release((unsigned long)p);
+// replace the operator "delete[]"
+void operator delete[](void *p) {
+    current_pool->release((unsigned long)p);
 }
 
 /*--------------------------------------------------------------------------*/
@@ -102,65 +98,64 @@ void operator delete[] (void * p) {
 
 /* -- EXAMPLE OF THE DIVISION-BY-ZERO HANDLER */
 
-void dbz_handler(REGS * r) {
-  Console::puts("DIVISION BY ZERO\n");
-  for(;;);
+void dbz_handler(REGS *r) {
+    Console::puts("DIVISION BY ZERO\n");
+    for (;;)
+        ;
 }
-
 
 /*--------------------------------------------------------------------------*/
 /* MAIN ENTRY INTO THE OS */
 /*--------------------------------------------------------------------------*/
 
 int main() {
-
-   GDT::init();
+    GDT::init();
     Console::init();
     IDT::init();
     ExceptionHandler::init_dispatcher();
     IRQ::init();
     InterruptHandler::init_dispatcher();
 
-    /* -- SEND OUTPUT TO TERMINAL -- */ 
+    /* -- SEND OUTPUT TO TERMINAL -- */
     Console::output_redirection(true);
 
     /* -- EXAMPLE OF AN EXCEPTION HANDLER -- */
-    
+
     class DBZ_Handler : public ExceptionHandler {
-      /* We derive Division-by-Zero handler from ExceptionHandler 
-         and overload the method handle_exception. */
-    public:
-        virtual void handle_exception(REGS * _regs) {
+        /* We derive Division-by-Zero handler from ExceptionHandler
+           and overload the method handle_exception. */
+       public:
+        virtual void handle_exception(REGS *_regs) {
             Console::puts("DIVISION BY ZERO!\n");
-            for(;;);
+            for (;;)
+                ;
         }
     } dbz_handler;
-    
-    /* Register the DBZ handler for exception no.0 
+
+    /* Register the DBZ handler for exception no.0
        with the exception dispatcher. */
     ExceptionHandler::register_handler(0, &dbz_handler);
-    
 
     /* -- INITIALIZE THE TIMER (we use a very simple timer).-- */
-    
+
     SimpleTimer timer(100); /* timer ticks every 10ms. */
-    
-    /* ---- Register timer handler for interrupt no.0 
+
+    /* ---- Register timer handler for interrupt no.0
             with the interrupt dispatcher. */
     InterruptHandler::register_handler(0, &timer);
-    
+
     /* NOTE: The timer chip starts periodically firing as
      soon as we enable interrupts.
      It is important to install a timer handler, as we
      would get a lot of uncaptured interrupts otherwise. */
-    
+
     /* -- INSTALL KEYBOARD HANDLER -- */
     SimpleKeyboard::init();
 
     Console::puts("after installing keyboard handler\n");
 
     /* -- ENABLE INTERRUPTS -- */
-    
+
     Machine::enable_interrupts();
 
     /* -- INITIALIZE FRAME POOLS -- */
@@ -169,11 +164,11 @@ int main() {
                                   KERNEL_POOL_SIZE,
                                   0);
 
-    unsigned long n_info_frames = 
-      ContFramePool::needed_info_frames(PROCESS_POOL_SIZE);
+    unsigned long n_info_frames =
+        ContFramePool::needed_info_frames(PROCESS_POOL_SIZE);
 
-    unsigned long process_mem_pool_info_frame = 
-      kernel_mem_pool.get_frames(n_info_frames);
+    unsigned long process_mem_pool_info_frame =
+        kernel_mem_pool.get_frames(n_info_frames);
 
     ContFramePool process_mem_pool(PROCESS_POOL_START_FRAME,
                                    PROCESS_POOL_SIZE,
@@ -187,12 +182,12 @@ int main() {
     /* ---- INSTALL PAGE FAULT HANDLER -- */
 
     class PageFault_Handler : public ExceptionHandler {
-      /* We derive the page fault handler from ExceptionHandler 
-	 and overload the method handle_exception. */
-      public:
-      virtual void handle_exception(REGS * _regs) {
-        PageTable::handle_fault(_regs);
-      }
+        /* We derive the page fault handler from ExceptionHandler
+           and overload the method handle_exception. */
+       public:
+        virtual void handle_exception(REGS *_regs) {
+            PageTable::handle_fault(_regs);
+        }
     } pagefault_handler;
 
     /* ---- Register the page fault handler for exception no. 14
@@ -237,7 +232,7 @@ int main() {
 
     /* ---- We define a 256MB heap that starts at 1GB in virtual memory. -- */
     VMPool heap_pool(1 GB, 256 MB, &process_mem_pool, &pt1);
-    
+
     /* -- NOW THE POOLS HAVE BEEN CREATED. */
 
     Console::puts("VM Pools successfully created!\n");
@@ -258,50 +253,52 @@ int main() {
 }
 
 void GeneratePageTableMemoryReferences(unsigned long start_address, int n_references) {
-  // This tests just the page table. 
-  int *foo = (int *) start_address;
-  
-  for (int i=0; i<n_references; i++) {
-    foo[i] = i;
-  }
-  
-  Console::puts("DONE WRITING TO MEMORY. Now testing...\n");
+    // This tests just the page table.
+    int *foo = (int *)start_address;
 
-  for (int i=0; i<n_references; i++) {
-    if(foo[i] != i) {
-      TestFailed();
+    for (int i = 0; i < n_references; i++) {
+        foo[i] = i;
     }
-  }
+
+    Console::puts("DONE WRITING TO MEMORY. Now testing...\n");
+
+    for (int i = 0; i < n_references; i++) {
+        if (foo[i] != i) {
+            TestFailed();
+        }
+    }
 }
 
 void GenerateVMPoolMemoryReferences(VMPool *pool, int size1, int size2) {
-  // Here we test the VMPool 
-   current_pool = pool;
-   for(int i=1; i<size1; i++) {
-      int *arr = new int[size2 * i];
-      if(pool->is_legitimate((unsigned long)arr) == false) {
-         TestFailed();
-      }
-      for(int j=0; j<size2*i; j++) {
-         arr[j] = j;
-      }
-      for(int j=size2*i - 1; j>=0; j--) {
-         if(arr[j] != j) {
+    // Here we test the VMPool
+    current_pool = pool;
+    for (int i = 1; i < size1; i++) {
+        int *arr = new int[size2 * i];
+        if (pool->is_legitimate((unsigned long)arr) == false) {
             TestFailed();
-         }
-      }
-      delete[] arr;
-   }
+        }
+        for (int j = 0; j < size2 * i; j++) {
+            arr[j] = j;
+        }
+        for (int j = size2 * i - 1; j >= 0; j--) {
+            if (arr[j] != j) {
+                TestFailed();
+            }
+        }
+        delete[] arr;
+    }
 }
 
 void TestFailed() {
-   Console::puts("Test Failed\n");
-   Console::puts("YOU CAN TURN OFF THE MACHINE NOW.\n");
-   for(;;);
+    Console::puts("Test Failed\n");
+    Console::puts("YOU CAN TURN OFF THE MACHINE NOW.\n");
+    for (;;)
+        ;
 }
 
 void TestPassed() {
-   Console::puts("Test Passed! Congratulations!\n");
-   Console::puts("YOU CAN SAFELY TURN OFF THE MACHINE NOW.\n");
-   for(;;);
+    Console::puts("Test Passed! Congratulations!\n");
+    Console::puts("YOU CAN SAFELY TURN OFF THE MACHINE NOW.\n");
+    for (;;)
+        ;
 }
