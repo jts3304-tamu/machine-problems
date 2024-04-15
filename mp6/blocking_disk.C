8a42289a63192585eaa32d63db6d6d504ca61478
Jackson Stewart <jts3304@gmail.com>
2024-04-14 23:04:09 -0500

    implement blocking disk

--- mp6/blocking_disk.C
+++ mp6/blocking_disk.C
@@ -1,10 +1,10 @@
 /*
      File        : blocking_disk.c
 
-     Author      : 
-     Modified    : 
+     Author      :
+     Modified    :
 
-     Description : 
+     Description :
 
 */
 
@@ -12,37 +12,72 @@
 /* DEFINES */
 /*--------------------------------------------------------------------------*/
 
-    /* -- (none) -- */
+/* -- (none) -- */
 
 /*--------------------------------------------------------------------------*/
 /* INCLUDES */
 /*--------------------------------------------------------------------------*/
 
 #include "assert.H"
-#include "utils.H"
-#include "console.H"
 #include "blocking_disk.H"
+#include "console.H"
+#include "scheduler.H"
+
+#ifdef _USES_SCHEDULER_
+
+extern Scheduler* SYSTEM_SCHEDULER;
+
+#endif
 
 /*--------------------------------------------------------------------------*/
 /* CONSTRUCTOR */
 /*--------------------------------------------------------------------------*/
 
-BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size) 
-  : SimpleDisk(_disk_id, _size) {
+BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size)
+    : SimpleDisk(_disk_id, _size) {
 }
 
 /*--------------------------------------------------------------------------*/
 /* SIMPLE_DISK FUNCTIONS */
 /*--------------------------------------------------------------------------*/
 
-void BlockingDisk::read(unsigned long _block_no, unsigned char * _buf) {
-  // -- REPLACE THIS!!!
-  SimpleDisk::read(_block_no, _buf);
+void BlockingDisk::read(unsigned long _block_no, unsigned char* _buf) {
+    SimpleDisk::issue_operation(DISK_OPERATION::READ, _block_no);
 
+#ifdef _USES_SCHEDULER_
+    // yield until disk is ready (polling)
+    // this is not busy waiting, but rather a way to yield the CPU
+    // without having to implement a blocked queue
+    while (!SimpleDisk::is_ready()) SYSTEM_SCHEDULER->yield();
+#endif
+
+#ifndef _USES_SCHEDULER_
+    SimpleDisk::wait_until_ready();
+#endif
+
+    // read data from port
+    for (int i = 0; i < 256; i++) {
+        unsigned short data = Machine::inportw(0x1F0);
+        _buf[i * 2] = (unsigned char)data;
+        _buf[i * 2 + 1] = (unsigned char)(data >> 8);
+    }
 }
 
+void BlockingDisk::write(unsigned long _block_no, unsigned char* _buf) {
+    SimpleDisk::issue_operation(DISK_OPERATION::WRITE, _block_no);
 
-void BlockingDisk::write(unsigned long _block_no, unsigned char * _buf) {
-  // -- REPLACE THIS!!!
-  SimpleDisk::write(_block_no, _buf);
+#ifdef _USES_SCHEDULER_
+    // yield until disk is ready (polling)
+    while (!SimpleDisk::is_ready()) SYSTEM_SCHEDULER->yield();
+#endif
+
+#ifndef _USES_SCHEDULER_
+    SimpleDisk::wait_until_ready();
+#endif
+
+    // write data to port
+    for (int i = 0; i < 256; i++) {
+        unsigned short data = _buf[i * 2] | (_buf[i * 2 + 1] << 8);
+        Machine::outportw(0x1F0, data);
+    }
 }
