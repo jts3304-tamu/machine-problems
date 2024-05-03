1612db9b0f1a2d2f8b142c7a54f1803b5c3f1f6c
Jackson Stewart <jts3304@gmail.com>
2024-05-03 18:40:33 -0500

    tweak compilation

--- mp7/copykernel.sh
+++ mp7/copykernel.sh
@@ -1,5 +1,12 @@
 # Replace "/mnt/floppy" with the whatever directory is appropriate.
-sudo mount -o loop dev_kernel_grub.img /mnt/floppy
-sudo cp kernel.bin /mnt/floppy
-sleep 1s
-sudo umount /mnt/floppy
+# sudo mount -o loop dev_kernel_grub.img /mnt/floppy
+# sudo cp kernel.bin /mnt/floppy
+# sleep 1s
+# sudo umount /mnt/floppy
+
+mkdir -p ../mnt/floppy
+hdiutil attach -mountpoint ../mnt/floppy dev_kernel_grub.img
+cp kernel.bin ../mnt/floppy/ # do i need this?
+sleep 1
+hdiutil detach ../mnt/floppy
+rm -rf ../mnt/floppy
